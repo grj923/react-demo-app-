8a9bd08a40ae86aebae4e4f29fd61f054ae70151
Greg Johnson <grj923@gmail.com>
2021-01-28 14:46:41 -0500

    react-router added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React from "react";
+import { Route, route } from "react-router-dom";
 import Welcome from "./components/Welcome/Welcome";
 import Clock from "./components/Clock/Clock";
 import Contact from "./components/Contact/Contact";
+import Navigation from "./components/navigation/Navigation";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Welcome name="Greg" />
-      <Clock />
-      <Contact />
-      <h2>Welcome To React!</h2>
+      <Navigation />
+      <Route
+        exact
+        path="/"
+        render={(props) => <Welcome {...props} name="Greg" />}
+      />
+      <Route path="/Clock" component={Clock} />
+      <Route path="/Contact" component={Contact} />
     </div>
   );
 }
